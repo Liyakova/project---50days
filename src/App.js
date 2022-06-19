97cd246e1ac263c738458f987867bc26afe437ec
Julia <105552026+Liyakova@users.noreply.github.com>
2022-06-19 17:05:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './Data';
+import { useState } from 'react';
 
 function App() {
+
+  const [toDo, setToDo] = useState(data);
+  const [start, setStart] = useState(true);
+
+  const deleteElement = (id) => {
+    let block = toDo.filter(element => element.id !==id);
+    setToDo(block)
+  }
+
+  const deleteOrStart = () => {
+    setStart(!start);
+    start ? setToDo([]) : setToDo(data);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>PROJECT 50 CHALLENGE</h1>
+
+      <div className='block'>
+        {toDo.map(item => {
+        const {id, rule, image} = item;
+          return (
+            <div key={id} className='toDo'>
+              <h2>{id} - {rule}</h2>
+              <img src={image} alt='inspiration'/>
+              <button className='buttonDone' onClick={()=> deleteElement(id)}> ✓ </button>
+            </div>
+          )
+        }
+      )}
+      </div>
+
+      <button className='deleteAll' onClick={() => deleteOrStart()}>{start? "DONE!!!" : "START AGAIN"}</button>
+      <a href='https://medium.com/axis/project-50-328982584d98' target="_blank" rel="noreferrer">link</a>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,49 @@
+export const data = [
+    {
+        id: 1,
+        rule: "Wake up before 8 AM",
+        image: "https://images.unsplash.com/photo-1592133308400-19f5da311e91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+    },
+
+    {
+        id: 2,
+        rule: "Morning routine: 1 hr no distraction",
+        image: "https://miro.medium.com/max/648/1*FFCvLWyXACf__PjN_iLpGw.jpeg",
+    },
+
+    {
+        id: 3,
+        rule: "Excercise for 1 hr",
+        image: "https://miro.medium.com/max/648/1*P5bA-P1ZijEbdMtDuGdAcQ.jpeg",
+    },
+
+    {
+        id: 4,
+        rule: "Read 10 pages a day",
+        image: "https://images.unsplash.com/photo-1595448821620-d4d342b1a3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+    },
+
+    {
+        id: 5,
+        rule: "Dedicate 1 hr towards a new skill",
+        image: "https://images.unsplash.com/photo-1617035969033-a5c113b6f0d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+    },
+
+    {
+        id: 6,
+        rule: "Follow a healthy diet",
+        image: "https://images.unsplash.com/photo-1620414895999-94b018eab0bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+    },
+
+    {
+        id: 7,
+        rule: "Meditation",
+        image: "https://images.unsplash.com/photo-1554244933-d876deb6b2ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+    },
+
+    {
+        id: 8,
+        rule: "Track your daily progress in a journal",
+        image: "https://images.unsplash.com/photo-1471970394675-613138e45da3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+    },
+]
